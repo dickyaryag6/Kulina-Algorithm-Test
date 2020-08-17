424030fbe4b42c1868e08d715ca272538c49ae0a
Dicky arya Kesuma <dickyaryakesuma@gmail.com>
2020-08-18 01:41:01 +0700

    Add files via upload

---
+++ kulina_algorithm_test.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// FUNGSI NOMOR 1
+func sockMerchant(socks []int) int {
+	pairOfSocks := map[int]int{}
+	result := 0
+	for _, sock := range socks {
+		_, isExist := pairOfSocks[sock]
+		if isExist {
+			pairOfSocks[sock]++
+			if pairOfSocks[sock] % 2 == 0 {
+				result++
+			}
+		} else {
+			pairOfSocks[sock] = 1
+		}
+	}
+
+	return result
+}
+
+// FUNGSI NOMOR 2
+func countingValleys(steps []string) int {
+	level_before := 0
+    level := 0
+	result := 0
+	for _, step := range steps {
+		level_before = level
+		if step == "U" {
+			level++
+		} else if step == "D" {
+			level--
+			if level_before == 0 {
+				result++
+			}
+		}
+	}
+	return result
+}
+
+// FUNGSI NOMOR 3
+func printZeros(num int){
+	s :=strconv.Itoa(num)
+	length := len(s)-1
+	for _, char := range s {
+		zeros := ""
+		for j := 0; j < length; j++ {
+			zeros = zeros + string("0")
+		}
+		length--
+		fmt.Println(string(char)+zeros)
+	}
+}
+
+
+func changeBool(n bool) bool {
+	if n == true {
+		return false
+	} else {
+		return true
+	}
+}
+
+// FUNGSI NOMOR 4
+func lampSwitch(numOfLamps int) []bool {
+	lamps := make([]bool, numOfLamps)
+	for i := 0; i < numOfLamps; i++ {
+		for j := 0; j < numOfLamps; j++ {
+			if (j+1) % (i+1) == 0 {
+				lamps[j] = changeBool(lamps[j])
+			}
+		}
+	}
+	fmt.Println(lamps)
+	return lamps
+}
+
+func main() {
+	// NOMOR 1
+	socks := []int{10,20,20,10,10,30,50,10,20}
+	sockMerchant(socks)
+
+	// NOMOR 2
+	steps := []string{"U", "D", "D", "U", "U", "D", "D", "U"}
+	countingValleys(steps)
+
+	// NOMOR 3
+	number := 1345679
+	printZeros(number)
+
+	// NOMOR 4
+	numOfLamps := 100
+	lampSwitch(numOfLamps)
+}
